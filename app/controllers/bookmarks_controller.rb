285c4a908c2987ffca6885113f6044028829e2f4
ibrahima kaba <ikkaba92@hotmail.fr>
2022-04-23 18:25:21 +0200

    watch list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.find(params[:list_id])
+    @bookmark.destroy
+    if @bookmark.destroy
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
