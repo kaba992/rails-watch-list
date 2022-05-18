c4951b7f7050a59677bc240eaaa0b2ee22759809
Ibrahima Kaba <ikkaba92@hotmail.fr>
2022-05-18 23:29:33 +0200

    setup nouveau pc

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,10 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
-HOSTNAME = ENV['TMDB_API_KEY']
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
