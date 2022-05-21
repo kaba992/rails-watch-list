806ab932d2a21fd13f7c19da558f7011343a40a9
Ibrahima Kaba <ikkaba92@hotmail.fr>
2022-05-21 23:27:51 +0200

    heroku

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ require "sprockets/railtie"
 
 # require "rails/test_unit/railtie"
 
-
+Bundler.require(*Rails.groups)
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
